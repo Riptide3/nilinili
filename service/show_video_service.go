708dabab181ae1be048522d3eee7cfbb081fab55
Riptide3 <riptide.yzy@gmail.com>
2020-04-22 17:43:58 +0800

    添加查看某个视频详情的功能

---
+++ service/show_video_service.go
@@ -0,0 +1,24 @@
+package service
+
+import (
+	"nilinili/model"
+	"nilinili/serializer"
+)
+
+// ShowVideoService 视频详情的服务
+type ShowVideoService struct {
+}
+
+// Show 视频详情
+func (service *ShowVideoService) Show(id string) serializer.Response {
+	var video model.Video
+	err := model.DB.First(&video, id).Error
+	if err != nil {
+		return serializer.Response{
+			Code: 404,
+			Msg: "视频不存在",
+			Error: err.Error(),
+		}
+	}
+	return serializer.BuildVideoResponse(video)
+}
